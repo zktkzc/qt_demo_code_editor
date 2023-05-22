ded197dcd20f488b1f71290bda940170e03e2549
tkzc00 <2238983030@qq.com>
2023-05-22 16:52:42 +0800

    水平滚动条绑定

--- mytextedit.cpp
+++ mytextedit.cpp
@@ -6,9 +6,24 @@ MyTextEdit::MyTextEdit(QWidget *parent) :
     ui(new Ui::MyTextEdit)
 {
     ui->setupUi(this);
+    connect(ui->textEdit->horizontalScrollBar(), SIGNAL(valueChanged(int)), this, SLOT(TextEditHScrollBarChanged()));
+    connect(ui->horizontalScrollBar, SIGNAL(valueChanged(int)), this, SLOT(ScrollBarChanged()));
 }
 
 MyTextEdit::~MyTextEdit()
 {
     delete ui;
+}
+
+void MyTextEdit::TextEditHScrollBarChanged()
+{
+    ui->horizontalScrollBar->setMaximum(ui->textEdit->horizontalScrollBar()->maximum());
+    ui->horizontalScrollBar->setMinimum(ui->textEdit->horizontalScrollBar()->minimum());
+    ui->horizontalScrollBar->setPageStep(ui->textEdit->horizontalScrollBar()->pageStep());
+    ui->horizontalScrollBar->setValue(ui->textEdit->horizontalScrollBar()->value());
+}
+
+void MyTextEdit::ScrollBarChanged()
+{
+    ui->textEdit->horizontalScrollBar()->setValue(ui->horizontalScrollBar->value());
 }
--- mytextedit.h
+++ mytextedit.h
@@ -15,6 +15,10 @@ public:
     explicit MyTextEdit(QWidget *parent = nullptr);
     ~MyTextEdit();
 
+private slots:
+    void TextEditHScrollBarChanged();
+    void ScrollBarChanged();
+
 private:
     Ui::MyTextEdit *ui;
 };
